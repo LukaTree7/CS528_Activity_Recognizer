be593c906605d9947acedb478a4f271b26291784
LukaTree7 <sfang4@wpi.edu>
2025-03-06 13:56:45 -0500

    steps counting changes

--- app/src/main/java/com/example/project_3/MainActivity.kt
+++ app/src/main/java/com/example/project_3/MainActivity.kt
@@ -177,9 +177,10 @@ fun MotionDetectionScreen(
                             val cadence = deltaSteps / deltaTimeSec
                             val newStatus = when {
                                 cadence == 0.0.toFloat() -> "Still"
-                                cadence > 2.5 -> "Running"
-                                cadence > 5 -> "In Vehicle"
-                                else -> "Walking"
+                                cadence > 7.0 -> "In Vehicle"
+                                cadence in 2.5 .. 7.0 -> "Running"
+                                cadence in 1.0 .. 2.5 -> "Walking"
+                                else -> "Still"
                             }
                             if (newStatus != currentActivity) {
                                 val durationMillis = currentTime - activityStartTime
